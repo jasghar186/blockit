ca6fbdc905ad79bc16ce54a8d2be9b5bc0696135
Junaid Asghar <jasghar186@gmail.com>
2022-05-13 15:21:01 +0500

    second commit

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -21,6 +21,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<h1> this is header</h1>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
